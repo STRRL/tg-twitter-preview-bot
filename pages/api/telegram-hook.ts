d47649eb8921c8315d07e7b6c569a25a446a4c3b
STRRL <im@strrl.dev>
2022-09-24 12:23:11 +0800

    chore: the echo bot
    Signed-off-by: STRRL <im@strrl.dev>

---
+++ pages/api/telegram-hook.ts
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { CallTracker } from "assert";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Telegraf } from "telegraf";
+
+type Data = {
+  name: string;
+};
+
+const BOT_TOKEN: string = process.env.BOT_TOKEN!;
+const BASE_PATH: string = process.env.BASE_PATH!;
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.on("message", async (ctx) => {
+  await ctx.reply(JSON.stringify(ctx.message));
+});
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const { body } = req;
+    await bot.handleUpdate(body);
+  } catch (e) {
+    console.log(e);
+  }
+
+  res.status(200);
+}
